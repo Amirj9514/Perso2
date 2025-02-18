21a7827af759cc0c1dfcd9a0bb41c6b5284d824e
umair <umairjaveed345@gmail.com>
2025-02-18 15:00:13 +0500

    chnages

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  isPasswordVisible: boolean = false;
+export class LoginComponent implements OnInit {
 
+  isPasswordVisible: boolean = false;
+  loginForm!: FormGroup;
+  formSubmit: boolean = false;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+
+  }
   togglePasswordVisibility(): void {
     this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  onSubmit() {
+    this.formSubmit = true;
+
   }
 }
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -4,13 +4,20 @@ import { AvatarModule } from 'primeng/avatar';
 import { SvgIconDirective } from '../../../Shared/directives/svg-icon.directive';
 import { TooltipModule } from 'primeng/tooltip';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PrimeIcons } from 'primeng/api';
 @Component({
   selector: 'app-sidebar',
   standalone:true,
-  imports: [DividerModule ,AvatarModule , SvgIconDirective , TooltipModule , RouterModule],
+  imports: [DividerModule, AvatarModule, TooltipModule, RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
 
+  dropdownOpen = false;
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
